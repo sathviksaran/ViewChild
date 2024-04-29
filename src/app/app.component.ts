17926bd307e920f1f664f1cc17f0a9a2af0e847e
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2024-04-29 17:16:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DemoComponent } from "./demo/demo.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, DemoComponent]
 })
 export class AppComponent {
   title = 'ViewChild';
+
+  @ViewChild('dobInput') dateOfBirth!: ElementRef;
+  @ViewChild('ageInput') age!: ElementRef;
+  @ViewChild(DemoComponent, {static: true}) demoComp!: DemoComponent;
+
+  calculateAge(){
+    let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    let age = currentYear - birthYear;
+    this.age.nativeElement.value = age;
+    // console.log(this.dateOfBirth);
+    // console.log(this.age);
+  }
 }
